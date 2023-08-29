51b5366d840d9bbe638e17a42701567e84ed60e0
kevin stal <kevin.stal@gmx.ch>
2023-08-29 11:10:27 +0200

    island validations

--- app/models/island.rb
+++ app/models/island.rb
@@ -4,5 +4,5 @@ class Island < ApplicationRecord
   has_many :reviews, through: :bookings
 
   validates :title, :address, :description, presence: :true
-  validates :price_per_night, numericality: { only_integer: true }
+  validates :price_per_night, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
