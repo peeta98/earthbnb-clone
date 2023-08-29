dd81096db279168575e21e3b93c2d389272ff8a8
Ayub Hassan <ayyubahmed555@gmail.com>
2023-08-29 14:16:06 +0100

    Creating update method for islands

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -8,4 +8,19 @@ class IslandsController < ApplicationController
   def show
     @island = Island.find(params[:id])
   end
+  def edit
+    @island = Island.find(params[:id])
+  end
+  def update
+    @island = Island.find(params[:id])
+    if @island.update(island_params)
+      redirect_to island_path(@island), notice: "Island was successfully updated"
+    else 
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  private
+  def island_params
+    params.require(:island).permit(:title, :address, :description, :price_per_night )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :islands, only: %i[index show]
+  resources :islands, only: %i[index show edit update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
