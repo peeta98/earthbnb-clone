b50b0f83449de825048a2248adb2a5c422664626
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-08-29 12:56:57 +0000

    Basic new and create actions for islands

--- app/models/island.rb
+++ app/models/island.rb
@@ -2,7 +2,8 @@ class Island < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
+  has_many_attached :photos
 
   validates :title, :address, :description, presence: true
-  validates :price_per_night, numericality: { only_integer: true }
+  validates :price_per_night, presence: true, numericality: { only_integer: true }
 end
