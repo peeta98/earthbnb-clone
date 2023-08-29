b6d3fbd814e080443ecaf7f14dc1d455f1b4d8c6
Ayub Hassan <ayyubahmed555@gmail.com>
2023-08-29 16:04:00 +0100

    Creating booking actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+    before_action :set_island, only: %i[create new edit update accept decline]
+
+    def new
+        @booking = Booking.new
+    end
+
+    def create
+        @booking = current_user.bookings.build(booking_params)
+        @booking.island = @island
+        if @booking.save
+          redirect_to @booking, notice: 'Booking requested.'
+        else
+          render :new
+        end
+      end
+
+
+      private
+    
+      def booking_params
+        params.require(:booking).permit(:start_time, :end_time)
+      end
+      
+      def set_island
+        @island = Island.find(params[:island_id])
+      end
 end
