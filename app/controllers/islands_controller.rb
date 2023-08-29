9c6b0ae49446a532e8768117bbd900b6b388c7ba
Ayub Hassan <ayyubahmed555@gmail.com>
2023-08-29 11:20:57 +0100

    Adding show feature

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,2 +1,6 @@
 class IslandsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index, :show]
+    def show
+        @island = Island.find(params[:id])
+    end
 end
