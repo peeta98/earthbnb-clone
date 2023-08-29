98077088b250c6b87fbdde94d2a3d05472bbbc59
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2023-08-29 15:29:07 +0200

    created card to display on the index

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,5 +1,4 @@
 class IslandsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @islands = Island.all
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+  
   def home
   end
 end
