6e88f7bc2a272e3ac7656561deac99124ccf70d8
Ayub Hassan <ayyubahmed555@gmail.com>
2023-08-30 15:17:55 +0100

    Created dependencies

--- app/models/island.rb
+++ app/models/island.rb
@@ -2,8 +2,8 @@ class Island < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
-  has_many :bookings
-  has_many :reviews, through: :bookings
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings, dependent: :destroy
   has_many_attached :photos
 
   validates :title, :address, :description, presence: true
