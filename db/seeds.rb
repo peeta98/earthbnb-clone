b15300e19d5b60f9f1019be5140382d4afc293a3
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-08-30 13:17:34 +0000

    seeds file done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
 
 puts "Deleting seeds..."
 User.destroy_all
@@ -14,10 +15,65 @@ Review.destroy_all
 puts "Seeds deleted!"
 
 puts "Creating seeds..."
-pedro = User.create!(username: "Peeta98", email: "pedro99@gmail.com", password: "secret")
-klevion = User.create!(username: "Klevion", email: "klevion99@gmail.com", password: "secret")
-User.create!(username: "Gonçalo", email: "Goncalo99@gmail.com", password: "secret")
-terceira = Island.create!(title: "Best island in the azores", address: "Portugal, Azores, Terceira", description: "Best Island in the Universe", price_per_night: 15, user:pedro)
-newbooking = Booking.create!(start_date: "29/08/2023", end_date: "30/08/2023", user: klevion, island: terceira)
-Review.create!(content: "Best island everrrrr", rating: 5.0, booking: newbooking)
+pedro = User.create!(username: "Peeta98", email: "pedro99@gmail.com", password: "secret") # User who owns the Island
+klevion = User.create!(username: "Klevion", email: "klevion99@gmail.com", password: "secret") # User who rents the Island
+terceira = Island.create!(title: "Terceira Island", address: "Terceira, Azores, Portugal", description: "Best Island in the Universe", price_per_night: 20000, user: pedro)
+url1 = Cloudinary::Utils.cloudinary_url("Terceira2_zvnbne")
+url2 = Cloudinary::Utils.cloudinary_url("Terceira4_v1kca1")
+url3 = Cloudinary::Utils.cloudinary_url("Terceira3_dapyar")
+url4 = Cloudinary::Utils.cloudinary_url("Terceira1_fw9goo")
+
+puts url1
+file = URI.open(url1)
+terceira.photos.attach(io: file, filename: "terceira.png", content_type: "image/png")
+file2 = URI.open(url2)
+terceira.photos.attach(io: file2, filename: "terceira2.png", content_type: "image/png")
+file3 = URI.open(url3)
+terceira.photos.attach(io: file3, filename: "terceira3.png", content_type: "image/png")
+file4 = URI.open(url4)
+terceira.photos.attach(io: file4, filename: "terceira4.png", content_type: "image/png")
+terceira.save!
+puts "1st Island done"
+
+terceira2 = Island.create!(title: "Terceira Island", address: "Terceira, Azores, Portugal", description: "Best Island in the Universe", price_per_night: 20000, user: pedro)
+file = URI.open(url1)
+terceira2.photos.attach(io: file, filename: "terceira.png", content_type: "image/png")
+file2 = URI.open(url2)
+terceira2.photos.attach(io: file2, filename: "terceira2.png", content_type: "image/png")
+file3 = URI.open(url3)
+terceira2.photos.attach(io: file3, filename: "terceira3.png", content_type: "image/png")
+file4 = URI.open(url4)
+terceira2.photos.attach(io: file4, filename: "terceira4.png", content_type: "image/png")
+terceira2.save!
+puts "2nd Island done"
+
+terceira3 = Island.create!(title: "Terceira Island", address: "Terceira, Azores, Portugal", description: "Best Island in the Universe", price_per_night: 20000, user: pedro)
+file = URI.open(url1)
+terceira3.photos.attach(io: file, filename: "terceira.png", content_type: "image/png")
+file2 = URI.open(url2)
+terceira3.photos.attach(io: file2, filename: "terceira2.png", content_type: "image/png")
+file3 = URI.open(url3)
+terceira3.photos.attach(io: file3, filename: "terceira3.png", content_type: "image/png")
+file4 = URI.open(url4)
+terceira3.photos.attach(io: file4, filename: "terceira4.png", content_type: "image/png")
+terceira3.save!
+puts "3nd Island done"
+
+terceira4 = Island.create!(title: "Terceira Island", address: "Terceira, Azores, Portugal", description: "Best Island in the Universe", price_per_night: 20000, user: pedro)
+file = URI.open(url1)
+terceira4.photos.attach(io: file, filename: "terceira.png", content_type: "image/png")
+file2 = URI.open(url2)
+terceira4.photos.attach(io: file2, filename: "terceira2.png", content_type: "image/png")
+file3 = URI.open(url3)
+terceira4.photos.attach(io: file3, filename: "terceira3.png", content_type: "image/png")
+file4 = URI.open(url4)
+terceira4.photos.attach(io: file4, filename: "terceira4.png", content_type: "image/png")
+terceira4.save!
+puts "4rd Island done"
+
+booking = Booking.create!(start_date: "30/08/2023", end_date: "2/09/2023", user: klevion, island: terceira)
+booking2 = Booking.create!(start_date: "30/08/2023", end_date: "2/09/2023", user: klevion, island: terceira2)
+booking3 = Booking.create!(start_date: "30/08/2023", end_date: "2/09/2023", user: klevion, island: terceira3)
+booking4 = Booking.create!(start_date: "30/08/2023", end_date: "2/09/2023", user: klevion, island: terceira4)
+Review.create!(content: "Awesome island with a lot of cows!", rating: 5.0, booking:)
 puts "Seeds created!"
